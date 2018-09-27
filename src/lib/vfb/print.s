d3516c0ed5ab5b996468d381b585d8c32d055c4e
Alexandru-Paul Copil (copper) <alexandru.p.copil@gmail.com>
2018-09-27 15:36:19 +0100

    Implement vfb_printdump(*addr, len)
    Print n bytes from starting location address

--- src/lib/vfb/print.s
+++ src/lib/vfb/print.s
@@ -37,7 +37,7 @@ vfb_print:
 	MOV	R4, R0			@ Save string ptr in R4
 
 	LDR	R1, =vfb_data		@ Get vfb data address
-	LDR	R1, [R1]
+	LDR	R1, [R1]		@ Get vfb data
 
 	LSR	R6, R1, #24		@ Get grid X (R5) and Y (R6)
 	LSR	R5, R1, #16		@
@@ -110,3 +110,63 @@ vfb_println:
 	STRH	R1, [R0]		@ Update cursor
 
 	POP	{PC}			@ Return
+
+
+@ vfb_printdump(*addr, len)
+@ Prints 'len' (R1) bytes starting from a given address (R0).
+.globl vfb_printdump
+vfb_printdump:
+
+	PUSH	{R4-R9, LR}
+
+	curs_x		.req R7
+	curs_y		.req R8
+	grid_x		.req R5
+	grid_y		.req R6
+
+	ADD	R9, R0, R1		@ Save end   address
+	MOV	R4, R0			@ Save start address
+
+	LDR	R1, =vfb_data		@ Get vfb data address
+	LDR	R1, [R1]		@ Get vfb data
+
+	LSR	R6, R1, #24		@ Get grid X (R5) and Y (R6)
+	LSR	R5, R1, #16		@
+	AND	R5, R5, #0xFF		@
+
+	LSR	R8, R1, #8		@ Get curs X (R7) and Y (R8)
+	AND	R8, R8, #0xFF		@
+	AND	R7, R1, #0xFF		@
+
+ _vfb_printdump:
+
+	LDRB	R0, [R4], #1		@ Load char
+
+	CMP	curs_x, grid_x		@ Check X out of bounds
+	MOVHS	curs_x, #0		@ Wrap to new line
+	ADDHS	curs_y, #1		@
+	CMP	curs_y, grid_y		@ Check Y out of bounds
+	MOVHS	curs_x, #0		@ Wrap to top of screen
+	MOVHS	curs_y, #0		@ TODO: Scroll one line
+
+	LSL	R1, curs_x, #3		@ Transform grid space X to pixel space
+	LSL	R2, curs_y, #4		@ Transform grid space Y to pixel space
+	BL	draw_char		@ Draw char
+
+
+	ADD	curs_x, #1		@ Next position
+
+	CMP	R4, R9			@ Check for end
+	BNE	_vfb_printdump		@ Next char
+
+ _vfb_printdump_end:
+	LDR	R0, =vfb_data		@ Get vfb data address
+	STRB	curs_x, [R0, #0]	@ Save new curs x
+	STRB	curs_y, [R0, #1]	@ Save new curs y
+
+	.unreq curs_x
+	.unreq curs_y
+	.unreq grid_x
+	.unreq grid_y
+
+	POP	{R4-R9, PC}		@ Return
