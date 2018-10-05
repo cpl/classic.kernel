ed87d3d9d0a99dcdc8afd24c1ed012ad36d6e49f
Alexandru-Paul Copil (copper) <alexandru.p.copil@gmail.com>
2018-10-05 19:38:57 +0100

    Add starting headers for C code

---
+++ include/c/vfb.h
@@ -0,0 +1,20 @@
+/*
+   Copyright 2018 Alexandru-Paul Copil
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+extern const void* vfb_print(char* str);
+extern const void* vfb_println(char* str);
+extern const void* vfb_printdump(char* str, u32 len);
