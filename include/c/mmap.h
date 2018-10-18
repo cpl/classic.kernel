08989ff375b161917840b1d1919c617e3db1bba1
Alexandru-Paul Copil (copper) <alexandru.p.copil@gmail.com>
2018-10-18 17:57:48 +0100

    Add mmap

---
+++ include/c/mmap.h
@@ -0,0 +1,44 @@
+/*
+   Copyright 2018 Alexandru-Paul Copil
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/* mmap.h - Memory map of physical pages
+
+Map and free physical pages.
+
+*/
+
+
+#ifndef _INC_MMAP_H
+#define _INC_MMAP_H
+
+#define MM_PHYS_SYS 0x20000000
+#define MM_PHYS_GPU 0x04000000
+#define MM_PHYS_KNL 0x00100000
+#define MM_PHYS_USR (MM_PHYS_SYS-MM_PHYS_GPU-MM_PHYS_KNL)
+
+#define MM_SECT_SHIFT 20
+#define MM_PAGE_SHIFT 12
+
+#define MM_SECT_SIZE (1<<MM_SECT_SHIFT)
+#define MM_PAGE_SIZE (1<<MM_PAGE_SHIFT)
+
+#define MM_PAGES_TTL (MM_PHYS_USR/MM_PAGE_SIZE)
+
+
+void* mmap_aloc_page();
+void  mmap_free_page(void* addr);
+
+#endif
---
+++ src/sys/memory/mmap.c
@@ -0,0 +1,49 @@
+/*
+   Copyright 2018 Alexandru-Paul Copil
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+#include "types.h"
+#include "mmap.h"
+
+
+static u8  MMAP_PHYS_PAGES[MM_PAGES_TTL] = {0,};
+static u32 MMAP_FREE_PP  = 0;
+u32 MMAP_ALOC_PAGE_COUNT = 0;
+
+
+void* mmap_aloc_page() {
+    for(u32 index = MMAP_FREE_PP; index < MM_PAGES_TTL; index++)
+        if(MMAP_PHYS_PAGES[index] == 0) {
+            MMAP_PHYS_PAGES[index] = 1;
+            MMAP_FREE_PP = index+1;
+            MMAP_ALOC_PAGE_COUNT++;
+
+            return (void*)(MM_PHYS_KNL+(index<<MM_PAGE_SHIFT));
+        }
+
+    return NULL;
+}
+
+
+void mmap_free_page(void* addr) {
+    u32 index = ((u32)addr-MM_PHYS_KNL)>>MM_PAGE_SHIFT;
+
+    MMAP_PHYS_PAGES[index] = 0;
+    MMAP_ALOC_PAGE_COUNT--;
+
+    if(index < MMAP_FREE_PP)
+        MMAP_FREE_PP = index;
+}
